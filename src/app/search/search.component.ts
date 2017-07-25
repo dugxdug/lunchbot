b389ce342fd1a6b9959edc2a89619419eebdb4d2
Douglas Coble <dcoble@douglass-mbp.relias.int>
2017-07-25 17:28:23 -0400

    working on autocomplete method for maps api

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AgmCoreModule, MapsAPILoader } from '@agm/core';
 
 @Component({
   selector: 'app-search',
@@ -14,16 +16,44 @@ export class SearchComponent implements OnInit {
   // directionsService = new google.maps.DirectionsService();
   // radius: any;
 
-  title: string = 'My first AGM project';
-  lat: number = 35.775194;
-  lng: number = -78.647124;
+  title = 'My first AGM project';
+  lat = 35.775194;
+  lng = -78.647124;
 
-  constructor() { }
+  searchTerm = '';
+  google: any;
+  markers = [];
+
+  constructor(private _mapsAPILoader: MapsAPILoader, private _zone: NgZone) { }
 
   ngOnInit() {
+    // this._mapsAPILoader.load()
+    this.autocomplete();
   }
 
-  applyFilters() {
+  autocomplete() {
+  this._mapsAPILoader.load().then(() => {
+    const autocomplete = new this.google.maps.places.Autocomplete(document.getElementById('autocompleteInput'), {});
+    this.google.maps.event.addListener(this.autocomplete, 'place_changed', () => {
+      this._zone.run(() => {
+        const place = autocomplete.getPlace();
+
+        this.markers.push({
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng(),
+          label: place.name,
+        });
+
+        this.lat = place.geometry.location.lat();
+        this.lng = place.geometry.location.lng();
+
+        console.log(place);
+      });
+    });
+  });
+}
+
+  locationSearch() {
     return null;
   }
 }
